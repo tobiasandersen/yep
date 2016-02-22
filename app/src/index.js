d183ca7f4b3e20dcadf05095492041328ffcfd73
tobiasandersen <j.tobiasandersen@gmail.com>
2016-02-22 18:38:00 +0100

    Serve index.html using html-webpack-plugin

--- app/src/index.js
+++ app/src/index.js
@@ -2,4 +2,5 @@ import React from 'react'
 import { render } from 'react-dom'
 import App from 'components/App'
 
+document.body.innerHTML += '<div id="root"></div>'
 render(<App />, document.getElementById('root'))
