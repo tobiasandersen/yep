755e34827350cfd6b6b771b505141fbdbbbc5d5f
Axel Niklasson <axel.niklasson@live.com>
2016-02-24 15:21:54 +0100

    Implemented API client for questions API

--- server/src/main/java/edu/dat076/yep/controllers/CategoryController.java
+++ server/src/main/java/edu/dat076/yep/controllers/CategoryController.java
@@ -1,6 +1,9 @@
 package edu.dat076.yep.controllers;
 
 import edu.dat076.yep.models.Category;
+import edu.dat076.yep.repositories.CardRepository;
+import edu.dat076.yep.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,14 +17,17 @@ import java.util.List;
 @RestController
 public class CategoryController {
 
+    @Autowired
+    private CategoryRepository repository;
+
     @RequestMapping(value="/categories", method=RequestMethod.GET)
     public List<Category> findAllCategories() {
-        return null;
+        return (List<Category>) repository.findAll();
     }
 
     @RequestMapping(value="/categories/{categoryID}", method=RequestMethod.GET)
     public Category findCategoryByID(@PathVariable int categoryID) {
-        return null;
+        return repository.findOne((long) categoryID);
     }
 
 }
--- server/src/main/java/edu/dat076/yep/repositories/CardRepository.java
+++ server/src/main/java/edu/dat076/yep/repositories/CardRepository.java
@@ -2,6 +2,8 @@ package edu.dat076.yep.repositories;
 
 import edu.dat076.yep.models.Card;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by marcus on 2016-02-23.
