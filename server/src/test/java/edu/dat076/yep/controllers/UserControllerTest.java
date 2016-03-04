372c43d01d0c5e5eb9c53118f57f9e20b7f7c48c
Marcus Trigell <leppmannen@gmail.com>
2016-03-04 16:34:25 +0100

    wrote test for UserController

---
+++ server/src/test/java/edu/dat076/yep/controllers/UserControllerTest.java
@@ -0,0 +1,7 @@
+package edu.dat076.yep.controllers;
+
+/**
+ * Created by marcus on 2016-03-03.
+ */
+public class UserControllerTest {
+}
---
+++ server/src/test/java/edu/dat076/yep/models/UserTest.java
@@ -0,0 +1,7 @@
+package edu.dat076.yep.models;
+
+/**
+ * Created by marcus on 2016-03-04.
+ */
+public class UserTest {
+}
