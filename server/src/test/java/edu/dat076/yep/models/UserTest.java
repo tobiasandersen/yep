4551f8724b5ef06a67dded90641895342861eea8
Marcus Trigell <leppmannen@gmail.com>
2016-03-04 18:43:25 +0100

    test classes now runs as expected

--- server/src/test/java/edu/dat076/yep/models/UserTest.java
+++ server/src/test/java/edu/dat076/yep/models/UserTest.java
@@ -1,7 +1,35 @@
 package edu.dat076.yep.models;
 
+import edu.dat076.yep.YepApplicationTests;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
 /**
  * Created by marcus on 2016-03-04.
+ *
+ * Tests the User class.
  */
-public class UserTest {
+@Transactional
+public class UserTest extends YepApplicationTests {
+
+    @Before
+    public void setUp() {
+
+    }
+
+    @After
+    public void tearDown() {
+
+    }
+
+    @Test
+    public void testGetName() {
+        User user = new User("test");
+
+        Assert.assertTrue(user.getName().equals("test"));
+    }
 }
