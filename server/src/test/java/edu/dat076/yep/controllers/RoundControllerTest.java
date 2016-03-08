f4e419cc675bfd03455f555e54d6ea7a2c5b613f
Marcus Trigell <leppmannen@gmail.com>
2016-03-08 09:07:42 +0100

    added POST to round controller

--- server/src/test/java/edu/dat076/yep/controllers/RoundControllerTest.java
+++ server/src/test/java/edu/dat076/yep/controllers/RoundControllerTest.java
@@ -1,8 +1,14 @@
 package edu.dat076.yep.controllers;
 
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
 import edu.dat076.yep.YepApplicationTests;
 import edu.dat076.yep.models.Round;
 import edu.dat076.yep.repositories.RoundRepository;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.Assert;
 import org.junit.After;
 import org.junit.Before;
@@ -45,17 +51,31 @@ public class RoundControllerTest extends YepApplicationTests {
         Assert.assertNotNull("failure - expected a list of rounds", list);
     }
 
-/*    @Test
-    public void testCreateRound() {
-        String json = "";
-        Round newRound = controller.createRound(json);
-        Assert.assertNotNull(newRound);
-        Assert.assertEquals(MULTI, newRound.getMultiplier());
-    }*/
-
     @Test
     public void testFindRoundByID() {
         Round round = controller.findRoundByID(ID);
         Assert.assertNotNull("failure - expected a round with id " + ID, round);
+    }
+
+    @Test
+    public void testCreateRound() {
+        Round newRound = null;
+        int multi = 0;
+        String message = "failure - expected newly created Round object";
+        try {
+            // Response is from an online json generator
+            HttpResponse response = Unirest.get("http://www.json-generator.com/api/json/get/cuSAnquKHm?indent=2").asJson();
+            JSONObject jsonObject = new JSONObject(response.getBody());
+            String json = jsonObject.toString();
+            newRound = controller.createRound(json);
+
+            //multi = jsonObject.getInt("multiplier");
+        } catch(UnirestException e) {
+            message = "failure - unirest exception got caught";
+        } catch(JSONException e) {
+            message = "failure - json exception got caught";
+        }
+        Assert.assertNotNull(message, newRound);
+        //Assert.assertEquals(multi, newRound.getMultiplier());
     }
 }
